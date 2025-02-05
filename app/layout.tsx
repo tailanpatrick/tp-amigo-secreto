c23d3053058583a8f309550cf8578079eae12aab
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-05 15:57:31 -0300

    next and shadcn installed

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Tp Amigo Secreto",
+  description: "Site para sorteio de Amigo Secreto.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`dark`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div className="bg-background w-screen h-screen">Home</div>
+  );
+}
