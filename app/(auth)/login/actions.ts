f02773193a914b4a735cdf3c322af9bcc14178a5
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-06 14:24:32 -0300

    create actions of login page

---
+++ app/(auth)/login/actions.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import { createClient } from '@/utils/supabase/server'
+import { LoginState } from '@/types/LoginState';
+
+export async function login(previousState: LoginState, formData: FormData) {
+  const supabase = await createClient()
+
+  const email = formData.get("email") as string;
+
+  const { error } = await supabase.auth.signInWithOtp({
+    email,
+    options: {
+      emailRedirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/confirm`
+    }
+  });
+
+  if (error) {
+    return {
+      success: false,
+      message: error.message
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Link enviado por email'
+  }
+}
