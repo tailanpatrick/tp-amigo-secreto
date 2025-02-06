71889482ca49be1a1e96351dc51b27fd95fed71c
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-06 14:25:42 -0300

    create login page container

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/login-form";
+
+const LoginPage = () => {
+    return (  
+        <div className="flex h-screen w-full  items-center justify-center p-4">
+            <LoginForm/>
+        </div>
+    );
+}
+ 
+export default LoginPage;
