3d5bfbd99aa69255de4556a9c5977a87e453a897
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-07 12:48:26 -0300

    created layout for logged users

---
+++ app/application/layout.tsx
@@ -0,0 +1,30 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+import Header from "@/components/header";
+
+const AppLayout = async ({
+  children
+}:
+  Readonly<{
+    children: React.ReactNode
+  }>
+) => {
+
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  // if(error || data?.user) {
+  //   redirect('/login');
+  // }
+
+  return (
+    <div>
+      <Header/>
+      {children}
+    </div>
+  );
+}
+
+export default AppLayout;
