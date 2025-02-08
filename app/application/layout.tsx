c35a20367c2e577f078ef6ae1f60a84d16ff0891
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-08 11:56:52 -0300

    create layout of all application

--- app/application/layout.tsx
+++ app/application/layout.tsx
@@ -15,13 +15,13 @@ const AppLayout = async ({
 
   const { data, error } = await supabase.auth.getUser();
 
-  // if(error || data?.user) {
-  //   redirect('/login');
-  // }
+  if (error || !data?.user) {
+    redirect('/login');
+  }
 
   return (
     <div>
-      <Header/>
+      <Header />
       {children}
     </div>
   );
