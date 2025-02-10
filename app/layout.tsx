93df43d26e1cfa582d85cfde7aeb3656783149e9
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-02-10 16:41:51 -0300

    add toast to root layout.tsx of project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Tp Amigo Oculto",
@@ -17,6 +18,7 @@ export default function RootLayout({
         className={`dark bg-background`}
       >
         {children}
+        <Toaster/>
       </body>
     </html>
   );
